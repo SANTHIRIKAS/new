2149e0b410fafbe31d8c38cd591a53f1502237d9
SANTHIRIKAS <sekarkamatchi1985@gmail.com>
2025-04-03 22:15:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CounterApp from './CounterApp';  // Import the CounterApp component
+
+function App() {
+  return (
+    <div className="App">
+      <CounterApp />  {/* Render the CounterApp component */}
+    </div>
+  );
+}
+
+export default App;
